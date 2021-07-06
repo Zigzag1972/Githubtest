18ba59c275fb9707d626141904ee157f552e6a7e
Zigzag1972 <87021780+Zigzag1972@users.noreply.github.com>
2021-07-06 10:39:25 -0400

    Testing BigIP
    rtew

---
+++ Test.ts
@@ -0,0 +1,30 @@
+provider "bigip" {
+    address = "x.x.x.x"
+    username = "xxxx"
+    password = "xxxx"
+}
+
+resource "bigip_ltm_policy" "test-policy" {
+    name = "my_policy"
+    strategy = "first-match"
+    requires = ["http"]
+    published_copy = "Drafts/my_policy"
+    controls = ["forwarding"]
+    rule {
+        name = "rule6"
+        action = {
+            tm_name = "20"
+            forward = true
+            pool = "/Common/mypool"
+        }
+    }
+    depends_on = ["bigip_ltm_pool.mypool"]
+}
+
+resource "bigip_ltm_pool" "mypool" {
+    name = "/Common/mypool"
+    monitors = ["/Common/http"]
+    allow_nat = "yes"
+    allow_snat = "yes"
+    load_balancing_mode = "round-robin"
+}
